69c8e37fe9e48389a8a35dce46091b69ceaf7304
Rihin <rihin@example.com>
2026-01-07 16:49:34 +0530

    enable swagger docs

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Nebula Backend",
+    version="1.0.0"
+)
 
 @app.get("/")
 def root():
     return {"status": "API is live 🚀"}
+
+@app.get("/ping")
+def ping():
+    return {"ping": "pong"}
